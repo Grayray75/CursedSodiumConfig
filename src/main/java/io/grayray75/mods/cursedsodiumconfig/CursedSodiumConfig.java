2edd3c42d6003047bb2e26bf5ab39123c43e5740
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2023-08-09 20:56:59 +0200

    Fix resource path in production

--- src/main/java/io/grayray75/mods/cursedsodiumconfig/CursedSodiumConfig.java
+++ src/main/java/io/grayray75/mods/cursedsodiumconfig/CursedSodiumConfig.java
@@ -2,29 +2,29 @@ package io.grayray75.mods.cursedsodiumconfig;
 
 import io.grayray75.mods.cursedsodiumconfig.config.VideoConfigIO;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-@Environment(EnvType.CLIENT)
 public class CursedSodiumConfig implements ClientModInitializer {
-
+    private static Logger Logger;
     public static String WinFormsExe;
 
     @Override
     public void onInitializeClient() {
+        Logger = LoggerFactory.getLogger("CursedSodiumConfig");
+
         try {
+            Logger.info("Extracting WinForms files...");
             this.extractWinForms();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -36,34 +36,24 @@ public class CursedSodiumConfig implements ClientModInitializer {
         Path modDataDir = Paths.get(dataDir.toString(), "cursedsodiumconfig");
         Files.createDirectories(modDataDir);
 
-        List<String> files = this.getResourceFiles("WinForms");
+        List<String> files = Arrays.asList(
+                "CursedSodiumConfig.WinForms.deps.json",
+                "CursedSodiumConfig.WinForms.dll",
+                "CursedSodiumConfig.WinForms.exe",
+                "CursedSodiumConfig.WinForms.runtimeconfig.json");
 
         for (int i = 0; i < files.size(); i++) {
             String fileName = files.get(i);
 
-            InputStream fileStream = CursedSodiumConfig.class.getClassLoader().getResourceAsStream("WinForms/" + fileName);
+            InputStream fileStream = CursedSodiumConfig.class.getClassLoader().getResourceAsStream("assets/cursedsodiumconfig/WinForms/" + fileName);
             Path filePath = Paths.get(modDataDir.toString(), fileName);
             Files.copy(fileStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            Logger.info("Extracted file: " + fileName);
 
             if (fileName.endsWith(".exe")) {
                 WinFormsExe = filePath.toString();
             }
         }
-    }
-
-    private List<String> getResourceFiles(String path) throws IOException {
-        List<String> filenames = new ArrayList<>();
-
-        try (InputStream in = CursedSodiumConfig.class.getClassLoader().getResourceAsStream(path);
-             BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            String resource;
-
-            while ((resource = br.readLine()) != null) {
-                filenames.add(resource);
-            }
-        }
-
-        return filenames;
     }
 
     public static Process openWinForms() {
